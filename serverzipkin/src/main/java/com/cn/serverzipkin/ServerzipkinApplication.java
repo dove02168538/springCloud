154766783426dea85d2ce517c23d9097671089c4
ergou <603900626@qq.com>
2018-06-20 10:22:11 +0800

    初步完善

---
+++ serverzipkin/src/main/java/com/cn/serverzipkin/ServerzipkinApplication.java
@@ -0,0 +1,12 @@
+package com.cn.serverzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerzipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerzipkinApplication.class, args);
+    }
+}
